91032dda336489f45ba3d5f78a70c2325b026f46
Matheus <matheusnascimentoeoq@gmail.com>
2020-09-12 13:27:45 -0200

    Correção do código

--- challenges/week 1 - leap years/matheussilva2.py
+++ challenges/week 1 - leap years/matheussilva2.py
@@ -1,3 +1,9 @@
-ano = int(input("Digite o ano: "))
-bisexto = (ano%4==0 and ano%100!=0) or (ano % 400 == 0)
-print(f"O ano {ano} é bisexto" if bisexto else f"O ano {ano} não é bisexto")
+ano1 = int(input("Digite o primeiro ano: "))
+ano2 = int(input("Digite o segundo ano: "))
+anos_bissextos = 0
+for ano in range(ano1,ano2+1):
+	bisexto = (ano%4==0 and ano%100!=0) or (ano % 400 == 0)
+	if(bisexto):
+		anos_bissextos += 1
+
+print(f"Entre {ano1} e {ano2}, existem {anos_bissextos} anos bissextos")
